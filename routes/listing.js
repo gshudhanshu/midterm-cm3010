59c9835582d7ecb8bc2f460a46fa903291384e9c
Shudhanshu Gunjal <33764308+gshudhanshu@users.noreply.github.com>
2023-07-31 12:55:58 +0530

    working on search

--- routes/listing.js
+++ routes/listing.js
@@ -6,11 +6,42 @@ const pool = require('../dbconnection')
  * @api {get} / All listings
  */
 router.get('/', async (req, res) => {
-  let query = 'SELECT * FROM listing'
-  pool.query(query, (err, result) => {
+  let currPage = parseInt(req.query.currPage) || 1
+  let offset = 15
+  let query = `SELECT * FROM listing
+   INNER JOIN listing_url ON listing.listing_url_id = listing_url.listing_url_id
+   LIMIT ${offset} OFFSET ${(currPage - 1) * offset}`
+
+  // Query to get the total count of records in the `listing` table
+  let countQuery = `SELECT COUNT(*) as total FROM listing`
+
+  pool.query(countQuery, (err, countResult) => {
     if (err) throw err
-    console.log(result)
-    // res.render('listings', { listings: result })
+    const totalPages = Math.ceil(countResult[0].total / offset)
+    console.log(totalPages)
+    pool.query(query, (err, result) => {
+      if (err) throw err
+      res.render('listings', {
+        listings: result,
+        pagination: {
+          currPage: currPage,
+          perPage: offset,
+          totalPages: totalPages,
+        },
+        pageInfo: { title: 'All listings' },
+      })
+    })
+  })
+})
+
+/**
+ * @api {get} /:id Listing by id
+ */
+router.get('/listing/:id', async (req, res) => {
+  let query = 'SELECT * FROM listing WHERE listing_id = ?'
+  pool.query(query, [req.params.id], (err, result) => {
+    if (err) throw err
+    res.render('listing-details', { listing: result[0] })
   })
 })
 
