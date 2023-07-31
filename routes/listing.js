c2b19ce213baf24b23fee1f4eb94864ecaf93956
Shudhanshu Gunjal <33764308+gshudhanshu@users.noreply.github.com>
2023-08-01 04:57:37 +0530

    listing_details complete

--- routes/listing.js
+++ routes/listing.js
@@ -51,10 +51,29 @@ router.get('/', async (req, res) => {
  * @api {get} /:id Listing by id
  */
 router.get('/listing/:id', async (req, res) => {
-  let query = 'SELECT * FROM listing WHERE listing_id = ?'
+  let query = `SELECT listing.*, listing_url.*, host.*, host_url.*, neighborhood.*, 
+              geo_location.*, property_type.*, room_type.*, 
+              GROUP_CONCAT(amenity.amenity SEPARATOR ', ') AS amenities
+              FROM listing
+              INNER JOIN listing_url ON listing.listing_url_id = listing_url.listing_url_id
+              INNER JOIN host ON listing.host_id = host.host_id
+              INNER JOIN host_url ON host.host_url_id = host_url.host_url_id
+              INNER JOIN neighborhood ON listing.neighborhood_id = neighborhood.neighborhood_id
+              INNER JOIN geo_location ON listing.geo_location_id = geo_location.geo_location_id
+              INNER JOIN property_type ON listing.property_type_id = property_type.property_type_id
+              INNER JOIN room_type ON listing.room_type_id = room_type.room_type_id
+              INNER JOIN listing_amenity_junction ON listing.listing_id = listing_amenity_junction.listing_id
+              INNER JOIN amenity ON listing_amenity_junction.amenity_id = amenity.amenity_id
+              WHERE listing.listing_id = ?
+              GROUP BY listing.listing_id
+              LIMIT 1`
   pool.query(query, [req.params.id], (err, result) => {
     if (err) throw err
-    res.render('listing-details', { listing: result[0] })
+    console.log(result[0])
+    res.render('listing-details', {
+      listing: result[0],
+      pageInfo: { title: 'Listing details' },
+    })
   })
 })
 
