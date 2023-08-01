e110150c2b1b24e960068e0aad3ddcdb4262ebf7
Shudhanshu Gunjal <33764308+gshudhanshu@users.noreply.github.com>
2023-08-01 15:48:32 +0530

    working on add listing

--- routes/listing.js
+++ routes/listing.js
@@ -3,18 +3,123 @@ const router = express.Router()
 const pool = require('../dbconnection')
 
 /**
- * @api {get} / Add new listing
+ * @api {get} /listing/add-listing
+ * Add new listing
  */
 router.get('/listing/add-listing', async (req, res) => {
-  const hosts = await pool.query('SELECT host_id, host_name FROM host ')
-  res.render('add-listing', {
-    pageInfo: { title: 'Add new listing' },
-    hosts: hosts[0],
-  })
+  console.log('test')
+  try {
+    const hosts = await pool.query('SELECT host_id, host_name FROM host')
+    const roomTypes = await pool.query(
+      'SELECT room_type_id, room_type FROM room_type'
+    )
+    const propertyTypes = await pool.query(
+      'SELECT property_type_id, property_type FROM property_type'
+    )
+
+    res.render('add-listing', {
+      pageInfo: { title: 'Add new listing' },
+      hosts: hosts[0],
+      room_types: roomTypes[0],
+      property_types: propertyTypes[0],
+    })
+  } catch (err) {
+    console.error('Error fetching data:', err)
+    res.status(500).send('Internal Server Error')
+  }
 })
 
 /**
- * @api {get} / All listings
+ * @api {post} /listing/add-listing Add new listing
+ */
+router.post('/listing/add-listing', async (req, res) => {
+  const {
+    name,
+    description,
+    neighborhood_overview,
+    amenities,
+    accommodates,
+    bedrooms,
+    beds,
+    latitude,
+    longitude,
+    bathrooms,
+    room_type,
+    property_type,
+    availability_365,
+    picture_url,
+    price,
+    host_id,
+  } = req.body
+
+  // As we don't have listing_url as it is a link to the listing on Airbnb,
+  // we'll hardcode it to a default value
+  const listing_url = 'https://www.airbnb.co.in'
+
+  const addListingUrlQuery = `INSERT INTO listing_url (listing_url, picture_url
+    ) VALUES (?, ?)`
+  const addNeighborhoodQuery = `INSERT INTO neighborhood (neighborhood) VALUES (?)`
+  const addGeoLocationQuery = ` INSERT INTO geo_location (latitude, longitude) VALUES (?, ?)`
+  const addPropertyTypeQuery = `INSERT INTO property_type (property_type) VALUES (?)`
+  const addRoomTypeQuery = `INSERT INTO room_type (room_type) VALUES (?)`
+  const addAmenityQuery = `INSERT INTO amenity (amenity) VALUES (?)`
+  const addReviewQuery = `INSERT INTO review
+                          (number_of_reviews, review_scores_rating)
+                          VALUES (?, ?)`
+
+  const listingUrlResult = await pool.query(addListingUrlQuery, [
+    listing_url,
+    picture_url,
+  ])
+  const neighborhoodResult = await pool.query(addNeighborhoodQuery, [
+    neighborhood,
+  ])
+  const geoLocationResult = await pool.query(addGeoLocationQuery, [
+    latitude,
+    longitude,
+  ])
+  const propertyTypeResult = await pool.query(addPropertyTypeQuery, [
+    property_type,
+  ])
+  const roomTypeResult = await pool.query(addRoomTypeQuery, [room_type])
+  const amenityResult = await pool.query(addAmenityQuery, [amenities])
+  const reviewResult = await pool.query(addReviewQuery, [
+    number_of_reviews,
+    review_scores_rating,
+  ])
+
+  // After inserting foreign table values, we can now insert into the listing table
+  const addListingQuery =
+    'INSERT INTO listing (name, description, neighborhood_overview, amenities, accommodates, bedrooms, beds, latitude, longitude, bathrooms, room_type_id, property_type_id, availability_365, picture_url, price, listing_url_id, host_id, neighborhood_id, geo_location_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+  const [listingResult] = await pool.query(addListingQuery, [
+    name,
+    description,
+    neighborhood_overview,
+    amenities,
+    accommodates,
+    bedrooms,
+    beds,
+    latitude,
+    longitude,
+    bathrooms,
+    room_type,
+    property_type,
+    availability_365,
+    picture_url,
+    price,
+    host_url_id,
+    host_url_id,
+    host_location_id,
+    host_location_id,
+  ])
+
+  // Assuming you have a success page, you can redirect to it
+  res.redirect('/listing/' + listingResult[0].insertId)
+})
+
+/**
+ * @api {get} /
+ * All listings
  */
 router.get('/', async (req, res) => {
   try {
@@ -61,7 +166,8 @@ router.get('/', async (req, res) => {
 })
 
 /**
- * @api {get} /:id Listing by id
+ * @api {get} /:id
+ * Listing by id
  */
 router.get('/listing/:id', async (req, res) => {
   try {
