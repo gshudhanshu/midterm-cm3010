83fdee4bbff619ea4817149aeee5688d288c729e
Shudhanshu Gunjal <33764308+gshudhanshu@users.noreply.github.com>
2023-08-02 18:28:57 +0530

    db port added

--- create_db.js
+++ create_db.js
@@ -10,6 +10,7 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   multipleStatements: true,
 })
 
--- populate_db.js
+++ populate_db.js
@@ -10,6 +10,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   connectionLimit: 5,
   multipleStatements: true,
 })
